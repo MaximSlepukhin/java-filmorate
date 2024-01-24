a69f98f0a91d5d19d0e0fbf48820bdfc55757620
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-01-25 01:02:47 +0300

    Доработал проект.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -4,7 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exceptions.*;
+import ru.yandex.practicum.filmorate.exceptions.ErrorResponse;
+import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.UserIdIsEmptyException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 
 @RestControllerAdvice
 public class ErrorHandler {
