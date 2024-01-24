fb0779682120b3ffc02f2df002f3a6b9be7e1357
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-01-25 01:19:36 +0300

    Доработал проект.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.IncorrectCountException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
