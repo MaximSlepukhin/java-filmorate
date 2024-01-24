ae0c0dcef9e17c4bde670d956f777fd49a5eaa81
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-01-25 01:42:23 +0300

    Доработал проект.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -55,9 +55,9 @@ public class FilmService {
     }
 
     public void deleteLike(Integer filmId, Integer userId) {
-        Integer idOfUser = userService.findUserById(userId).getId();
+        userService.checkId(userId);
         Film film = findFilmById(filmId);
-        film.likes.remove(idOfUser);
+        film.likes.remove(userId);
     }
 
     public Film findFilmById(Integer id) {
