73ecb6035629e90f3dbf0081254149c2e2fe7632
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-01-25 01:24:38 +0300

    Доработал проект.

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IncorrectCountException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IncorrectCountException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class IncorrectCountException extends RuntimeException{
+public class IncorrectCountException extends RuntimeException {
 
     public IncorrectCountException(String message) {
         super(message);
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserIdIsEmptyException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserIdIsEmptyException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class UserIdIsEmptyException extends RuntimeException{
+public class UserIdIsEmptyException extends RuntimeException {
     public UserIdIsEmptyException(String message) {
         super(message);
     }
