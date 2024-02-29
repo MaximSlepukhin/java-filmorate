4299768dbca0f10e40f51b0d4fa87acca8a1e5b1
Maxim Slepukhin <slepuhinmaksym@yandex.ru>
2024-02-29 21:07:23 +0300

    ФП11.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.*;
+
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
